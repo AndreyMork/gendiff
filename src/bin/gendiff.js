d294f48676952189ceb184201fdec7c4ccc149f7
Andrey <magot.a.d@gmail.com>
2018-03-13 06:43:23 +0300

    fix gendiff and change version

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,12 +1,14 @@
 #! /usr/bin/env node
 import program from 'commander';
 import gendiff from '../';
+import { version } from '../../package.json';
 
 program
   .description('Compares two configuration files and shows the difference.')
+  .version(version)
   .arguments('<firstConfig>')
   .arguments('<secondConfig>')
   .option('-V, --version', 'output the version number')
   .option('-f, --format [type]', 'output format')
-  .action((firstConfig, secondConfig) => gendiff(firstConfig, secondConfig))
+  .action((firstConfig, secondConfig) => console.log(gendiff(firstConfig, secondConfig)))
   .parse(process.argv);
