eb7af4c59db87374f9aa76ae52728ecc243d8c52
Andrey <magot.a.d@gmail.com>
2018-03-14 01:14:45 +0300

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 import { readFileSync } from 'fs';
-import { difference, union } from 'lodash';
+import { union } from 'lodash';
 
 const getObjFromJson = filepath => JSON.parse(readFileSync(filepath), (key, value) => value || '""');
 
@@ -8,29 +8,24 @@ export default (beforeFilePath, afterFilePath) => {
   const beforeFile = getObjFromJson(beforeFilePath);
   const afterFile = getObjFromJson(afterFilePath);
 
-  const beforeFileKeys = Object.keys(beforeFile);
-  const afterFileKeys = Object.keys(afterFile);
+  const isRemovedKey = key => beforeFile[key] && !afterFile[key];
+  const isAddedKey = key => !beforeFile[key] && afterFile[key];
+  const isChangedKey = key => beforeFile[key] !== afterFile[key];
 
-  const addedKeys = new Set(difference(afterFileKeys, beforeFileKeys));
-  const removedKeys = new Set(difference(beforeFileKeys, afterFileKeys));
+  const keys = union(Object.keys(beforeFile), Object.keys(afterFile));
+  const diffStrings = keys
+    .map((key) => {
+      const beforeStr = `${key}: ${beforeFile[key]}`;
+      const afterStr = `${key}: ${afterFile[key]}`;
 
-  const keyToString = (key) => {
-    const beforeStr = `${key}: ${beforeFile[key]}`;
-    const afterStr = `${key}: ${afterFile[key]}`;
+      if (isAddedKey(key)) {
+        return `  + ${afterStr}`;
+      } else if (isRemovedKey(key)) {
+        return `  - ${beforeStr}`;
+      }
 
-    if (addedKeys.has(key)) {
-      return `  + ${afterStr}`;
-    } else if (removedKeys.has(key)) {
-      return `  - ${beforeStr}`;
-    } else if (beforeStr === afterStr) {
-      return `    ${afterStr}`;
-    }
+      return isChangedKey(key) ? `  + ${afterStr}\n  - ${beforeStr}` : `    ${afterStr}`;
+    });
 
-    return `  + ${afterStr}\n  - ${beforeStr}`;
-  };
-
-  const keys = union(beforeFileKeys, afterFileKeys);
-  const diffStrings = keys.map(key => keyToString(key)).join('\n');
-
-  return `{\n${diffStrings}\n}\n`;
+  return ['{', ...diffStrings, '}\n'].join('\n');
 };
