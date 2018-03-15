88f9f5b06f32cbb167528866e530b6cb9e9608b8
Andrey <magot.a.d@gmail.com>
2018-03-15 22:13:41 +0300

    add ast tests

--- Makefile
+++ Makefile
@@ -14,5 +14,8 @@ lint:
 test:
 	npm run test
 
+ast-test:
+	npm run ast-test
+
 gendiff:
 	npm run babel-node -- src/bin/gendiff.js __tests__/__fixtures__/json_tests/all_before.json __tests__/__fixtures__/json_tests/all_after.json
---
+++ __tests__/ast.test.js
@@ -0,0 +1,49 @@
+import { readFileSync } from 'fs';
+import parse from '../src/parser';
+import ast from '../src/AST';
+
+const pathTo = filename => `${__dirname}/__fixtures__/ast_tests/${filename}`;
+const expected = [
+  {
+    key: 'common',
+    type: 'obj',
+    children: [
+      { key: 'setting1', type: 'common', value: 'Value 1' },
+      { key: 'setting2', type: 'remove', value: '200' },
+      // { key: 'setting3', type: 'remove', value: true },
+      // { key: 'setting3', type: 'add', value: { key: 'value' } },
+      { key: 'setting3', type: 'change', value: { before: true, after: { key: 'value' } } },
+      {
+        key: 'setting6',
+        type: 'obj',
+        children: [
+          { key: 'key', type: 'common', value: 'value' },
+          { key: 'ops', type: 'add', value: 'vops' },
+        ],
+      },
+      { key: 'setting4', type: 'add', value: 'blah blah' },
+      { key: 'setting5', type: 'add', value: { key5: 'value5' } },
+    ],
+  },
+  {
+    key: 'group1',
+    type: 'obj',
+    children: [
+      { key: 'baz', type: 'change', value: { before: 'bas', after: 'bars' } },
+      { key: 'foo', type: 'common', value: 'bar' },
+      // { key: 'nest', type: 'remove', value: { key: 'value' } },
+      // { key: 'nest', type: 'add', value: 'str' },
+      { key: 'nest', type: 'change', value: { before: { key: 'value' }, after: 'str' } },
+    ],
+  },
+  { key: 'group2', type: 'remove', value: { abc: '12345' } },
+  { key: 'group3', type: 'add', value: { fee: '100500' } },
+];
+
+
+test('json', () => {
+  const before = parse(readFileSync(pathTo('before.json'), 'utf-8'), '.json');
+  const after = parse(readFileSync(pathTo('after.json'), 'utf-8'), '.json');
+
+  expect(ast(before, after)).toEqual(expected);
+});
