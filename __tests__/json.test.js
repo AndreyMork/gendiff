1f9e8540c9d54a56420672d4181e2b15a488a495
Andrey <magot.a.d@gmail.com>
2018-03-16 05:19:04 +0300

    little fix tests

--- __tests__/json.test.js
+++ __tests__/json.test.js
@@ -1,29 +1,30 @@
 import { readFileSync } from 'fs';
+import { trimEnd } from 'lodash';
 import getDiff from '../src';
 
 const pathTo = filename => `${__dirname}/__fixtures__/json_tests/${filename}`;
 
 test('hexlet', () => {
-  const expected = readFileSync(pathTo('hexlet_expected'), 'utf-8');
+  const expected = trimEnd(readFileSync(pathTo('hexlet_expected'), 'utf-8'));
   expect(getDiff(pathTo('hexlet_before.json'), pathTo('hexlet_after.json'))).toBe(expected);
 });
 
 test('all', () => {
-  const expected = readFileSync(pathTo('all_expected'), 'utf-8');
+  const expected = trimEnd(readFileSync(pathTo('all_expected'), 'utf-8'));
   expect(getDiff(pathTo('all_before.json'), pathTo('all_after.json'))).toBe(expected);
 });
 
 test('remove', () => {
-  const expected = readFileSync(pathTo('remove_expected'), 'utf-8');
+  const expected = trimEnd(readFileSync(pathTo('remove_expected'), 'utf-8'));
   expect(getDiff(pathTo('remove_before.json'), pathTo('remove_after.json'))).toBe(expected);
 });
 
 test('add', () => {
-  const expected = readFileSync(pathTo('add_expected'), 'utf-8');
+  const expected = trimEnd(readFileSync(pathTo('add_expected'), 'utf-8'));
   expect(getDiff(pathTo('add_before.json'), pathTo('add_after.json'))).toBe(expected);
 });
 
 test('no-diff', () => {
-  const expected = readFileSync(pathTo('no_diff_expected'), 'utf-8');
+  const expected = trimEnd(readFileSync(pathTo('no_diff_expected'), 'utf-8'));
   expect(getDiff(pathTo('no_diff_before.json'), pathTo('no_diff_after.json'))).toBe(expected);
 });
--- __tests__/yaml.test.js
+++ __tests__/yaml.test.js
@@ -1,29 +1,30 @@
 import { readFileSync } from 'fs';
+import { trimEnd } from 'lodash';
 import getDiff from '../src';
 
 const pathTo = filename => `${__dirname}/__fixtures__/yaml_tests/${filename}`;
 
 test('hexlet', () => {
-  const expected = readFileSync(pathTo('hexlet_expected'), 'utf-8');
+  const expected = trimEnd(readFileSync(pathTo('hexlet_expected'), 'utf-8'));
   expect(getDiff(pathTo('hexlet_before.yaml'), pathTo('hexlet_after.yaml'))).toBe(expected);
 });
 
 test('all', () => {
-  const expected = readFileSync(pathTo('all_expected'), 'utf-8');
+  const expected = trimEnd(readFileSync(pathTo('all_expected'), 'utf-8'));
   expect(getDiff(pathTo('all_before.yaml'), pathTo('all_after.yaml'))).toBe(expected);
 });
 
 test('remove', () => {
-  const expected = readFileSync(pathTo('remove_expected'), 'utf-8');
+  const expected = trimEnd(readFileSync(pathTo('remove_expected'), 'utf-8'));
   expect(getDiff(pathTo('remove_before.yaml'), pathTo('remove_after.yaml'))).toBe(expected);
 });
 
 test('add', () => {
-  const expected = readFileSync(pathTo('add_expected'), 'utf-8');
+  const expected = trimEnd(readFileSync(pathTo('add_expected'), 'utf-8'));
   expect(getDiff(pathTo('add_before.yaml'), pathTo('add_after.yaml'))).toBe(expected);
 });
 
 test('no-diff', () => {
-  const expected = readFileSync(pathTo('no_diff_expected'), 'utf-8');
+  const expected = trimEnd(readFileSync(pathTo('no_diff_expected'), 'utf-8'));
   expect(getDiff(pathTo('no_diff_before.yaml'), pathTo('no_diff_after.yaml'))).toBe(expected);
 });
