019114260ef5aecea04ad9031d9461e6b65e868c
Andrey <magot.a.d@gmail.com>
2018-03-17 00:55:58 +0300

    refactor gendiff

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,25 +3,12 @@ import program from 'commander';
 import gendiff from '../';
 import { version } from '../../package.json';
 
-const actions = {
-  plain: (first, second) => console.log(gendiff(first, second, 'plain')),
-  json: (first, second) => console.log(gendiff(first, second, 'json')),
-};
-
 program
   .description('Compares two configuration files and shows the difference.\n  Supported formats: "json, yaml, ini".')
   .version(version)
   .arguments('<firstConfig>')
   .arguments('<secondConfig>')
   .option('-f, --format [type]', 'output format')
-  .action((firstConfig, secondConfig) => {
-    const action = actions[program.format];
-    if (!action) {
-      console.log(gendiff(firstConfig, secondConfig));
-    } else {
-      action(firstConfig, secondConfig);
-    }
-
-    return null;
-  })
+  .action((firstConfig, secondConfig) =>
+    console.log(gendiff(firstConfig, secondConfig, program.format)))
   .parse(process.argv);
