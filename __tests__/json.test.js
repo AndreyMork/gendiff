55b5260d6fa96b66b28787e8eb3b706f21529a51
Andrey <magot.a.d@gmail.com>
2018-03-16 18:38:38 +0300

    add plain test

--- __tests__/json.test.js
+++ __tests__/json.test.js
@@ -1,14 +1,25 @@
 import { readFileSync } from 'fs';
+import { trimEnd } from 'lodash';
 import getDiff from '../src';
 
 const pathTo = filename => `${__dirname}/__fixtures__/json_tests/${filename}`;
 
 test('hexletNested', () => {
-  const expected = readFileSync(pathTo('hexletNested_expected'), 'utf-8');
+  const expected = readFileSync(pathTo('hexletNested_json_expected'), 'utf-8');
   expect(getDiff(pathTo('hexletNested_before.json'), pathTo('hexletNested_after.json'))).toBe(expected);
 });
 
 test('all', () => {
-  const expected = readFileSync(pathTo('all_expected'), 'utf-8');
+  const expected = readFileSync(pathTo('all_json_expected'), 'utf-8');
   expect(getDiff(pathTo('all_before.json'), pathTo('all_after.json'))).toBe(expected);
+});
+
+test('hexletNested_plain', () => {
+  const expected = trimEnd(readFileSync(pathTo('hexletNested_plain_expected'), 'utf-8'));
+  expect(getDiff(pathTo('hexletNested_before.json'), pathTo('hexletNested_after.json'), 'plain')).toBe(expected);
+});
+
+test('all_plain', () => {
+  const expected = trimEnd(readFileSync(pathTo('all_plain_expected'), 'utf-8'));
+  expect(getDiff(pathTo('all_before.json'), pathTo('all_after.json'), 'plain')).toBe(expected);
 });
