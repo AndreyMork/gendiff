8d7aaac0c08c333cf1185779bc800ed43f3998e0
Andrey <magot.a.d@gmail.com>
2018-03-16 14:43:21 +0300

    refactor ast

--- src/ast.js
+++ src/ast.js
@@ -1,10 +1,9 @@
-import { union, has, isObject, flatten } from 'lodash';
+import { union, has, isObject } from 'lodash';
 
 const isBothObj = (key, beforeObj, afterObj) => isObject(beforeObj[key]) && isObject(afterObj[key]);
 const isRemoved = (key, beforeObj, afterObj) => has(beforeObj, key) && !has(afterObj, key);
 const isAdded = (key, beforeObj, afterObj) => !has(beforeObj, key) && has(afterObj, key);
 const isChanged = (key, beforeObj, afterObj) => beforeObj[key] !== afterObj[key];
-const oneIsObj = (key, beforeObj, afterObj) => isObject(beforeObj[key]) || isObject(afterObj[key]);
 
 const getKeyType = (key, beforeObj, afterObj) => {
   if (isBothObj(key, beforeObj, afterObj)) {
@@ -13,8 +12,6 @@ const getKeyType = (key, beforeObj, afterObj) => {
     return 'added';
   } else if (isRemoved(key, beforeObj, afterObj)) {
     return 'removed';
-  } else if (oneIsObj(key, beforeObj, afterObj)) {
-    return 'oneObj';
   }
 
   return isChanged(key, beforeObj, afterObj) ? 'changed' : 'common';
@@ -32,15 +29,14 @@ const makeAst = (beforeObj, afterObj) => {
       valueAfter: afterObj[key],
     }),
     nested: key => ({ key, type: 'nested', children: makeAst(beforeObj[key], afterObj[key]) }),
-    oneObj: key => [{ key, type: 'removed', value: beforeObj[key] }, { key, type: 'added', value: afterObj[key] }],
   };
 
   const keys = union(Object.keys(beforeObj), Object.keys(afterObj));
 
-  return flatten(keys.map((key) => {
+  return keys.map((key) => {
     const type = getKeyType(key, beforeObj, afterObj);
     return makeNode[type](key);
-  }));
+  });
 };
 
 export default makeAst;
