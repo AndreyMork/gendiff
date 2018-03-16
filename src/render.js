47e61e57ea4e7a716ea6a71edd2729873a3c995a
Andrey <magot.a.d@gmail.com>
2018-03-16 04:08:35 +0300

    at least it works

--- src/render.js
+++ src/render.js
@@ -1,25 +1,36 @@
-import { flatten, trimEnd } from 'lodash';
+import { flatten, trimEnd, isObject } from 'lodash';
 
 const indent = ' '.repeat(2);
 const doubleIndent = indent.repeat(2);
 
-const makeAddStr = (key, value) => trimEnd(`${indent}+ ${key}: ${value}`);
-const makeRemoveStr = (key, value) => trimEnd(`${indent}- ${key}: ${value}`);
-const makeCommonStr = (key, value) => trimEnd(`${doubleIndent}${key}: ${value}`);
+const objToString = (obj, level) => {
+  const keys = Object.keys(obj);
+  const strings = keys.map(key => `${doubleIndent.repeat(level)}${key}: ${isObject(obj[key]) ? objToString(obj[key], level + 1) : obj[key]}`);
 
-const getStr = {
-  add: (key, value) => makeAddStr(key, value),
-  remove: (key, value) => makeRemoveStr(key, value),
-  common: (key, value) => makeCommonStr(key, value),
-  change: (key, value) => [makeAddStr(key, value.after), makeRemoveStr(key, value.before)],
+  const res = ['{', ...strings, `${doubleIndent.repeat(level - 1)}}`].join('\n');
+
+  return res;
 };
 
-const render = (ast) => {
+const makeAddStr = (key, value, level) => trimEnd(`${indent.repeat((level * 2) - 1)}+ ${key}: ${isObject(value) ? objToString(value, level + 1) : value}`);
+const makeRemoveStr = (key, value, level) => trimEnd(`${indent.repeat((level * 2) - 1)}- ${key}: ${isObject(value) ? objToString(value, level + 1) : value}`);
+const makeCommonStr = (key, value, level) => trimEnd(`${doubleIndent.repeat(level)}${key}: ${isObject(value) ? objToString(value, level + 1) : value}`);
+
+const getStr = {
+  add: (key, value, level) => makeAddStr(key, value, level),
+  remove: (key, value, level) => makeRemoveStr(key, value, level),
+  common: (key, value, level) => makeCommonStr(key, value, level),
+  change: (key, value, level) => [makeAddStr(key, value.after, level),
+    makeRemoveStr(key, value.before, level)],
+  obj: (key, childrenAsStr, level) => `${doubleIndent.repeat(level)}${key}: ${childrenAsStr}`,
+};
+
+const render = (ast, level = 1) => {
   const strings = flatten(ast.map(({
     type, key, value, children,
   }) =>
-    (type === 'obj' ? getStr[type](key, render(children)) : getStr[type](key, value))));
-  return ['{', ...strings, '}\n'].join('\n');
+    (type === 'obj' ? getStr[type](key, render(children, level + 1), level) : getStr[type](key, value, level))));
+  return ['{', ...strings, `${doubleIndent.repeat(level - 1)}}${level === 1 ? '\n' : ''}`].join('\n');
 };
 
 export default render;
