d5613d45096eb4131a4e0f1778721a1607a4a20c
Andrey <magot.a.d@gmail.com>
2018-03-17 06:39:24 +0300

    change description

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,13 +2,14 @@
 import program from 'commander';
 import gendiff from '../';
 import { version } from '../../package.json';
+import { formatTypes } from '../render';
 
 program
   .description('Compares two configuration files and shows the difference.\n  Supported formats: "json, yaml, ini".')
   .version(version)
   .arguments('<firstConfig>')
   .arguments('<secondConfig>')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', `output format. Supported: ${formatTypes.join(', ')}.`)
   .action((firstConfig, secondConfig) =>
     console.log(gendiff(firstConfig, secondConfig, program.format)))
   .parse(process.argv);
