b7c4d37ae9014025f21264852ae5561e7c9bc5a0
Andrey <magot.a.d@gmail.com>
2018-03-17 06:44:23 +0300

    style

--- src/render.js
+++ src/render.js
@@ -11,7 +11,7 @@ export const formatTypes = Object.keys(renderers);
 export default (ast, format) => {
   const render = renderers[format];
   if (!render) {
-    return `'${format}' is unknown format type.\n format types: ${formatTypes.join(', ')}`;
+    return `'${format}' is unknown output format.\nOutput formats: ${formatTypes.join(', ')}`;
   }
 
   return render(ast);
