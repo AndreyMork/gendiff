0816d604d398b8c437c1f28e8a6d89c67ff2d44b
Andrey <magot.a.d@gmail.com>
2018-03-17 03:36:09 +0300

    chore tests

--- __tests__/ini.test.js
+++ __tests__/ini.test.js
@@ -4,22 +4,22 @@ import getDiff from '../src';
 
 const pathTo = filename => `${__dirname}/__fixtures__/ini_tests/${filename}`;
 
-test('hexletNested', () => {
+test('raw hexletNested', () => {
   const expected = readFileSync(pathTo('hexletNested_raw_expected'), 'utf-8');
   expect(getDiff(pathTo('hexletNested_before.ini'), pathTo('hexletNested_after.ini'))).toBe(expected);
 });
 
-test('all', () => {
+test('raw all', () => {
   const expected = readFileSync(pathTo('all_raw_expected'), 'utf-8');
   expect(getDiff(pathTo('all_before.ini'), pathTo('all_after.ini'))).toBe(expected);
 });
 
-test('hexletNested_plain', () => {
+test('plain hexletNested', () => {
   const expected = trimEnd(readFileSync(pathTo('hexletNested_plain_expected'), 'utf-8'));
   expect(getDiff(pathTo('hexletNested_before.ini'), pathTo('hexletNested_after.ini'), 'plain')).toBe(expected);
 });
 
-test('all_plain', () => {
+test('plain all', () => {
   const expected = trimEnd(readFileSync(pathTo('all_plain_expected'), 'utf-8'));
   expect(getDiff(pathTo('all_before.ini'), pathTo('all_after.ini'), 'plain')).toBe(expected);
 });
--- __tests__/yaml.test.js
+++ __tests__/yaml.test.js
@@ -4,22 +4,22 @@ import getDiff from '../src';
 
 const pathTo = filename => `${__dirname}/__fixtures__/yaml_tests/${filename}`;
 
-test('hexletNested', () => {
+test('raw hexletNested', () => {
   const expected = readFileSync(pathTo('hexletNested_raw_expected'), 'utf-8');
   expect(getDiff(pathTo('hexletNested_before.yaml'), pathTo('hexletNested_after.yaml'))).toBe(expected);
 });
 
-test('all', () => {
+test('raw all', () => {
   const expected = readFileSync(pathTo('all_raw_expected'), 'utf-8');
   expect(getDiff(pathTo('all_before.yaml'), pathTo('all_after.yaml'))).toBe(expected);
 });
 
-test('hexletNested_plain', () => {
+test('plain hexletNested', () => {
   const expected = trimEnd(readFileSync(pathTo('hexletNested_plain_expected'), 'utf-8'));
   expect(getDiff(pathTo('hexletNested_before.yaml'), pathTo('hexletNested_after.yaml'), 'plain')).toBe(expected);
 });
 
-test('all_plain', () => {
+test('plain all', () => {
   const expected = trimEnd(readFileSync(pathTo('all_plain_expected'), 'utf-8'));
   expect(getDiff(pathTo('all_before.yaml'), pathTo('all_after.yaml'), 'plain')).toBe(expected);
 });
