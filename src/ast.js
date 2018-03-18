495306fc7180841195bcd0bec5665d930b48a8a2
Andrey <magot.a.d@gmail.com>
2018-03-18 22:19:21 +0300

    refactor ast

--- src/ast.js
+++ src/ast.js
@@ -4,48 +4,40 @@ const types = [
   {
     type: 'nested',
     check: (key, beforeObj, afterObj) => isObject(beforeObj[key]) && isObject(afterObj[key]),
+    process: (key, beforeObj, afterObj, makeAst) => ({ key, type: 'nested', children: makeAst(beforeObj[key], afterObj[key]) }),
   },
   {
     type: 'added',
     check: (key, beforeObj, afterObj) => !has(beforeObj, key) && has(afterObj, key),
+    process: (key, beforeObj, afterObj) => ({ key, type: 'added', value: afterObj[key] }),
   },
   {
     type: 'removed',
     check: (key, beforeObj, afterObj) => has(beforeObj, key) && !has(afterObj, key),
+    process: (key, beforeObj) => ({ key, type: 'removed', value: beforeObj[key] }),
   },
   {
     type: 'changed',
     check: (key, beforeObj, afterObj) => beforeObj[key] !== afterObj[key],
-  },
-  {
-    type: 'common',
-    check: (key, beforeObj, afterObj) => beforeObj[key] === afterObj[key],
-  },
-];
-
-const getKeyType = (key, beforeObj, afterObj) => {
-  const { type } = find(types, ({ check }) => check(key, beforeObj, afterObj));
-  return type;
-};
-
-const makeAst = (beforeObj, afterObj) => {
-  const makeNode = {
-    added: key => ({ key, type: 'added', value: afterObj[key] }),
-    removed: key => ({ key, type: 'removed', value: beforeObj[key] }),
-    common: key => ({ key, type: 'common', value: afterObj[key] }),
-    changed: key => ({
+    process: (key, beforeObj, afterObj) => ({
       key,
       type: 'changed',
       valueBefore: beforeObj[key],
       valueAfter: afterObj[key],
     }),
-    nested: key => ({ key, type: 'nested', children: makeAst(beforeObj[key], afterObj[key]) }),
-  };
+  },
+  {
+    type: 'common',
+    check: (key, beforeObj, afterObj) => beforeObj[key] === afterObj[key],
+    process: (key, beforeObj, afterObj) => ({ key, type: 'common', value: afterObj[key] }),
+  },
+];
 
+const makeAst = (beforeObj, afterObj) => {
   const keys = union(Object.keys(beforeObj), Object.keys(afterObj));
   return keys.map((key) => {
-    const type = getKeyType(key, beforeObj, afterObj);
-    return makeNode[type](key);
+    const { process } = find(types, ({ check }) => check(key, beforeObj, afterObj));
+    return process(key, beforeObj, afterObj, makeAst);
   });
 };
 
