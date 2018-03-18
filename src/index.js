5e7487caa0644cb560dbcb22067be1b425de9929
AndreyMork <35698586+AndreyMork@users.noreply.github.com>
2018-03-18 12:11:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import makeAST from './ast';
 import render from './render';
 
 export default (beforeFilePath, afterFilePath, format = 'raw') => {
-  const beforeFile = readFileSync(beforeFilePath, 'utf-8');
-  const afterFile = readFileSync(afterFilePath, 'utf-8');
+  const beforeFileStr = readFileSync(beforeFilePath, 'utf-8');
+  const afterFileStr = readFileSync(afterFilePath, 'utf-8');
 
-  const parsedBeforeFile = parse(beforeFile, extname(beforeFilePath));
-  const parsedAfterFile = parse(afterFile, extname(afterFilePath));
+  const parsedBeforeFile = parse(beforeFileStr, extname(beforeFilePath));
+  const parsedAfterFile = parse(afterFileStr, extname(afterFilePath));
 
   const ast = makeAST(parsedBeforeFile, parsedAfterFile);
 
